6d1196be6fc5761ad681e49d576b267eb47440e4
Kubeec <kubicki.lukasz98@gmail.com>
2019-10-05 04:22:14 +0200

    start
    add some sprites and player movement with animations

---
+++ Aloneland/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f; // speed of the player
+
+
+    Rigidbody2D rb2d; // rigidbody 2d of the gameObject(Player)
+
+
+    public float Velocity
+    {
+        get
+        {
+            return rb2d.velocity.magnitude;
+        }
+    }
+
+    public float DirectionX
+    {
+        get
+        {
+            return rb2d.velocity.x < 0 ? -1 : 1;    
+        }
+    }
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        Vector2 direction = new Vector3(Input.GetAxis("Horizontal") , Input.GetAxis("Vertical"));
+
+        Debug.Log(direction);
+
+        rb2d.AddForce(direction * speed);
+
+    }
+
+}
---
+++ Aloneland/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    Player player;
+
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetFloat("velocity", player.Velocity / 5);
+
+
+        transform.localScale = new Vector3(player.DirectionX , transform.localScale.y , transform.localScale.z);
+
+        
+    }
+}
